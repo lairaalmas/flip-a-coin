917434436ff90441e3e5a9f683cd618c6218c93f
lairaalmas <lairaalmas@gmail.com>
2023-02-03 11:59:10 -0300

    initial commit

---
+++ script/index.js
@@ -0,0 +1,23 @@
+const botao = document.querySelector("#botao");
+const moeda = document.querySelector("#moeda");
+
+const generateValue = () => Math.round(Math.random());
+
+const flip = () => {
+  moeda.innerHTML = "";
+  moeda.classList.add("flip");
+  moeda.classList.remove("-bgDefault");
+  moeda.classList.add("-bgFlip");
+};
+
+botao.addEventListener("click", function () {
+  flip();
+  const result = generateValue() === 0 ? "heads" : "tails";
+  setTimeout(function () {
+    moeda.classList.remove("flip");
+    moeda.innerHTML = `<img src="../assets/${result}.svg"/><span>${result}</span>`;
+    moeda.classList.add("-bgDefault");
+    moeda.classList.remove("-bgFlip");
+    callback();
+  }, 800);
+});
